ab3f41c3f2af25040ef4f3fe2158565818252fad
z00294w <saloniv04@gmail.com>
2018-02-11 03:07:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/z00294w/navigationdrawer4/Hello.java
@@ -0,0 +1,18 @@
+package com.example.z00294w.navigationdrawer4;
+
+/**
+ * Created by z00294w on 2/11/18.
+ */
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Hello extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
